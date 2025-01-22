6c5526dc5a53c2b616f70a96c38ee012eb0e1ae9
Aryan Mathur <65492352+aryanmathur2003@users.noreply.github.com>
2025-01-21 17:54:25 -0800

    Home Page + Views and Models Set Up

--- Frontend/Frontend/Views/Home.swift
+++ Frontend/Frontend/Views/Home.swift
@@ -1,15 +1,31 @@
 //
-//  Home.swift
+//  ContentView.swift
 //  Frontend
 //
-//  Created by Aryan Mathur on 1/21/25.
+//  Created by Aryan Mathur on 1/20/25.
 //
 
 import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                HStack {
+                    Text("The New Gym **Logger**")
+                        .font(.system(size: 36))
+                        .padding(.trailing)
+                    Spacer()
+                    Image(systemName: "line.3.horizontal")
+                        .imageScale(.large)
+                        .padding()
+                        .frame(width:70, height:90)
+                        .overlay(RoundedRectangle(cornerRadius: 50).stroke().opacity(0.4))
+                }
+                
+            }
+            .padding(30)
+        }
     }
 }
 
