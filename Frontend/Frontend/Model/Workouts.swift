4e88e7e005ee783cd04cbb5ad3557a1a3ad75fbe
Aryan Mathur <65492352+aryanmathur2003@users.noreply.github.com>
2025-01-21 17:53:22 -0800

    Header - Main Page + Views

---
+++ Frontend/Frontend/Model/Workouts.swift
@@ -0,0 +1,18 @@
+//
+//  Workouts.swift
+//  Frontend
+//
+//  Created by Aryan Mathur on 1/21/25.
+//
+
+import SwiftUI
+
+struct Workouts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Workouts()
+}
---
+++ Frontend/Frontend/Views/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  Frontend
+//
+//  Created by Aryan Mathur on 1/21/25.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
